552ed87643885c55864216d994272d7591754a77
Luis Sotomayor <LSotomayor@futuredontics.com>
2014-01-27 15:42:46 -0800

    This is a change

--- TestConsoleApplication/TestConsoleApplication/Program.cs
+++ TestConsoleApplication/TestConsoleApplication/Program.cs
@@ -10,6 +10,8 @@ namespace TestConsoleApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+
+            Console.WriteLine("This is a test");
         }
     }
 }
